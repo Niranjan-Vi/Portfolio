7d4f330348273d5ef7b5db62df74b3c15688e381
Niranjan Vi <157526421+Niranjan-Vi@users.noreply.github.com>
2024-06-10 21:49:12 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+setInterval(function(){
+    var text=document.getElementById("ann");
+    if(text.innerHTML==="Webpages"){
+        text.innerHTML="Games";
+        text.style.color='blue';
+        
+    }
+    else{
+            text.innerHTML="Webpages";
+            text.style.color='blue';
+            text.style.fontWeight='bold';
+    }
+},1500);
+function mainfun(){
+    window.location.href="index.html";
+}
+
+button=document.getElementById('button');
+button.addEventListener('click',()=>{
+    window.location.href="https://drive.google.com/drive/folders/13EzK-dJmp6SIQ3D1NJBpPUS5bRZjhjWd?usp=drive_link";
+   });
+
+ios=document.getElementById('ios');
+ios.addEventListener('click',()=>{
+    window.location.href="https://nv-calc.netlify.app/";
+});
+xo=document.getElementById('xo');
+xo.addEventListener('click',()=>{
+    window.location.href="https://nv-x-o-game.netlify.app/";
+});
+
+
+
